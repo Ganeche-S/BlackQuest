3ffd5442e2f20ff0a827795d3087b20610333178
gsivasankar <gsivasankar@iut.univ-paris8.fr>
2021-02-15 18:20:46 +0100

    Update AnimationEnemy

--- Assets/Script/Trigger.cs
+++ Assets/Script/Trigger.cs
@@ -30,16 +30,21 @@ public class Trigger : Enemy
     	if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius) {
     		if(currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger) {
 	    		Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-/*	    		changeAnim(temp - transform.position);*/
+	    		changeAnim(temp - transform.position);
 	    		myRigidbody.MovePosition(temp);
 	    		ChangeState(EnemyState.walk);
+	    		anim.SetBool("trigger", true);
 	    	}
     	}
+    	else if(Vector3.Distance(target.position, transform.position) > chaseRadius) {
+	    	ChangeState(EnemyState.idle);
+	    	anim.SetBool("trigger", false);
+	    }
     }
 
-/*    private void SetAnimFloat(Vector2 setVector) {
+    private void SetAnimFloat(Vector2 setVector) {
     	anim.SetFloat("moveX", setVector.x);
-    	anim.SetFloat("moveY", setVector.y);
+    	/*anim.SetFloat("moveY", setVector.y);*/
     }
 
     private void changeAnim(Vector2 direction) {
@@ -51,15 +56,15 @@ public class Trigger : Enemy
     			SetAnimFloat(Vector2.left);
     		}
     	}
-    	else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
+/*    	else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
     		if(direction.y > 0) {
     			SetAnimFloat(Vector2.up);
     		}
     		else if(direction.y < 0) {
     			SetAnimFloat(Vector2.down);
     		}
-    	}
-    }*/
+    	}*/
+    }
     private void ChangeState(EnemyState newState) {
     	if(currentState != newState) {
     		currentState = newState;
